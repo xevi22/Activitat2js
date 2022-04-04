6b61aed707fc8faa4062885e0842b8e87e7e25cf
xevi22 <xavier.pares@cirvianum.cat>
2022-04-04 16:23:18 +0200

    aa

--- Activitat2/js/Activitat2.js
+++ Activitat2/js/Activitat2.js
@@ -12,7 +12,79 @@ addEventListener('load', ()=>{
         div.innerText=myNews[index];
         if(doFlaix){
             div.classList.add('brighten');
+            setTimeout(function(){
+                div.classList.remove('brighten');
+            }, 2000);
+        }
+        updateButtons(index);
+    }
+    const doRequest = () => {
+        let http_request=null;
+        if(window.XMLHttpRequest){
+            http_request=new window.XMLHttpRequest();
+        }else if(window.ActiveXObject){
+            http_request=new window.ActiveXObject('Microsoft.XMLHTTP');
+        }
 
+        http_request.onreadystatechange=function(){
+            if(http_request.readyState===4){
+                if(http_request.status===200){
+                    let newLength=myNews.push(getHour() + " - " +
+                        http_request.responseText);
+                    showNews(newLength-1,true);
+                }
+            }
+        }
+        http_request.open('GET','../servidor/generarContinguts.php',true);
+        http_request.send(null);
+
+    };
+
+    const beginRequests = () => {
+        myInterval=setInterval(doRequest, 10000);
+        document.getElementById('aturar').value='Aturar';
+    };
+
+    const stopRequests = () => {
+        clearInterval(myInterval);
+        myInterval=null;
+        document.getElementById('aturar').value='Iniciar';
+    };
+
+    function navigateNews(forwards){
+        stopRequests();
+        let index =  myNews.indexOf(document.getElementById('ticker').innerText);
+        if(forwards) {
+            showNews(index + 1);
+        }else{
+            showNews(index - 1);
         }
     }
+
+    doRequest();
+
+    beginRequests();
+
+    document.getElementById('seguent').
+    addEventListener('click',()=>{
+        navigateNews(true);
+
+    });
+
+    document.getElementById('anterior').
+    addEventListener('click',()=>{
+        navigateNews(false);
+
+    });
+
+    document.getElementById('aturar').
+    addEventListener('click',()=>{
+        if(myInterval){
+            stopRequests();
+        }else{
+            beginRequests();
+        }
+
+    });
+
 })
